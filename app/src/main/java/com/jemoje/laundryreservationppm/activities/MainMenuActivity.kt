a52b4ce3206d96992b67253be7b2934c0cb6d23f
Joseph Moje <joseph.moje@fourello.com>
2020-01-31 00:53:05 +0800

    3rd

--- app/src/main/java/com/jemoje/laundryreservationppm/activities/MainMenuActivity.kt
+++ app/src/main/java/com/jemoje/laundryreservationppm/activities/MainMenuActivity.kt
@@ -1,13 +1,34 @@
 package com.jemoje.laundryreservationppm.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import com.jemoje.laundryreservationppm.R
+import kotlinx.android.synthetic.main.activity_main_menu.*
+
 
 class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
+
+        btn_menu.setOnClickListener {
+
+
+            toggle()
+        }
+
+    }
+
+    private fun toggle() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
